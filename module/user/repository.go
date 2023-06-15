25f3714ad43182297c290236598319af9cbeb44e
IrfannMh <irfann.mmm4@gmail.com>
2023-06-15 09:00:48 +0700

    feat:register

--- module/user/repository.go
+++ module/user/repository.go
@@ -50,7 +50,7 @@ func (r repository) createRole(role *entity.Role) error {
 
 func (r repository) getUserAndRole(id uint) (*entity.User, error) {
 	var user entity.User
-	err := r.db.Preload("Role").Find(&user, id).Error
+	err := r.db.Preload("Role").First(&user, id).Error
 	if err != nil {
 		return nil, err
 	}
