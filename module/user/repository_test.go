308b6c4b57f62629265b1574d29d182bfea19b8a
RobyPurbaSakty26 <obykao26@gmail.com>
2023-07-10 08:19:17 +0700

    test: add test respository user save

--- module/user/repository_test.go
+++ module/user/repository_test.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"regexp"
 	"testing"
+	"time"
 	"trb-backend/module/entity"
 	"trb-backend/test"
 
@@ -77,6 +78,185 @@ func Test_repository_getAccessByRoleId(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getAccessByRoleId() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// func Test_repository_save(t *testing.T) {
+// 	type fields struct {
+// 		db *gorm.DB
+// 	}
+// 	type args struct {
+// 		user *entity.User
+// 	}
+
+// 	type testCase struct {
+// 		name    string
+// 		fields  fields
+// 		args    args
+// 		wantErr bool
+// 	}
+
+// 	var tests []testCase
+// 	mockQuery, mockDb := test.NewMockQueryDB(t)
+// 	name := "error"
+
+// 	data := &entity.User{
+// 		Fullname:   "Joe",
+// 		Username:   "Joe",
+// 		Email:      "Joe@mail.com",
+// 		Password:   "password",
+// 		RoleId:     1,
+// 		Active:     true,
+// 		InputFalse: 0,
+// 	}
+
+// 	user := args{
+// 		user: data,
+// 	}
+
+// 	f := fields{db: mockDb}
+// 	query := regexp.QuoteMeta("INSERT INTO `users` (`created_at`,`updated_at`,`deleted_at`,`fullname`,`username`,`email`,`password`,`role_id`,`active`,`input_false`) VALUES (?,?,?,?,?,?,?,?,?,?)")
+// 	err := errors.New("error")
+// 	mockQuery.ExpectQuery(query).WillReturnError(err)
+// 	tests = append(tests, testCase{
+// 		name:    name,
+// 		fields:  f,
+// 		args:    user,
+// 		wantErr: true,
+// 	})
+
+// 	name = "success"
+// 	// mockQuery.ExpectQuery(query).WillReturnRows(
+// 	// 	sqlmock.NewRows([]string{`created_at`, `updated_at`, `deleted_at`, `fullname`, `username`, `email`, `password`, `role_id`, `active`, `input_false`}).AddRow(time.Now(), time.Now(), "NULL", "Joe", "Joe", "joe@mail.com", "password", 1, true, 0),
+// 	// )
+
+// 	mockQuery.ExpectExec(query).WithArgs(
+// 		mock.Anything, // created_at
+// 		mock.Anything, // updated_at
+// 		mock.Anything, // deleted_at
+// 		data.Fullname,
+// 		data.Username,
+// 		data.Email,
+// 		data.Password,
+// 		data.RoleId,
+// 		data.Active,
+// 		data.InputFalse,
+// 	).WillReturnResult(sqlmock.NewResult(1, 1))
+// 	tests = append(tests, testCase{
+// 		name:    name,
+// 		fields:  f,
+// 		args:    user,
+// 		wantErr: false,
+// 	})
+
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			r := repository{
+// 				db: tt.fields.db,
+// 			}
+// 			if err := r.save(tt.args.user); (err != nil) != tt.wantErr {
+// 				t.Errorf("save() error = %v, wantErr %v", err, tt.wantErr)
+// 			}
+// 		})
+// 	}
+// }
+
+func Test_repository_save(t *testing.T) {
+	type fields struct {
+		db *gorm.DB
+	}
+	type args struct {
+		user *entity.User
+	}
+
+	type testCase struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}
+
+	var tests []testCase
+	mockQuery, mockDb := test.NewMockQueryDB(t)
+	name := "error"
+
+	data := &entity.User{
+		Fullname:   "Joe",
+		Username:   "Joe",
+		Email:      "Joe@mail.com",
+		Password:   "password",
+		RoleId:     1,
+		Active:     true,
+		Role:       entity.Role{},
+		InputFalse: 0,
+		Model: gorm.Model{
+			DeletedAt: gorm.DeletedAt{},
+			UpdatedAt: time.Now(),
+			CreatedAt: time.Now(),
+		},
+	}
+
+	user := args{
+		user: data,
+	}
+
+	// Set created_at and updated_at fields to the current time
+	// data.CreatedAt = time.Now()
+	// data.UpdatedAt = data.CreatedAt
+	// data.DeletedAt = gorm.DeletedAt{}
+
+	f := fields{db: mockDb}
+	query := regexp.QuoteMeta("INSERT INTO `users` (`created_at`,`updated_at`,`deleted_at`,`fullname`,`username`,`email`,`password`,`role_id`,`active`,`input_false`) VALUES (?,?,?,?,?,?,?,?,?,?)")
+	err := errors.New("error")
+	mockQuery.ExpectExec(query).WithArgs(
+		data.CreatedAt, // created_at
+		data.UpdatedAt, // updated_at
+		nil,            // deleted_at
+		data.Fullname,
+		data.Username,
+		data.Email,
+		data.Password,
+		data.RoleId,
+		data.Active,
+		data.InputFalse,
+	).WillReturnError(err)
+	tests = append(tests, testCase{
+		name:    name,
+		fields:  f,
+		args:    user,
+		wantErr: true,
+	})
+
+	name = "success"
+	mockQuery.ExpectExec(query).WithArgs(
+		data.CreatedAt, // created_at
+		data.UpdatedAt, // updated_at
+		nil,            // deleted_at
+		data.Fullname,
+		data.Username,
+		data.Email,
+		data.Password,
+		data.RoleId,
+		data.Active,
+		data.InputFalse,
+	).WillReturnResult(sqlmock.NewResult(1, 1))
+	tests = append(tests, testCase{
+		name:    name,
+		fields:  f,
+		args:    user,
+		wantErr: false,
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := repository{
+				db: tt.fields.db,
+			}
+			err := r.save(tt.args.user)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("save() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
